cec3e1fa06db92413d12507ae48bf2b07d8beb29
operhack <operpanayotis@gmail.com>
2025-07-25 20:15:35 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
+const express = require("express");
+const app = express();
 
+app.get("/roblox/customer312fd", (req, res) => {
+  res.json({ script1: "Hi roblox!" },
+          { script2: "Script 5" });
+});
 
 app.get("/roblox/customer348fd", (req, res) => {
   res.json({ script1: "Script Number 5" });
+});
+
+const PORT = process.env.PORT || 10000;
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
 });
