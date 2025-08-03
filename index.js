9246cd5aea54303fc954a791eb0346ed3ae587c6
operhack <operpanayotis@gmail.com>
2025-08-03 12:53:07 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,16 +6,6 @@ const axios = require("axios");
 
 const url = "https://opergamertection.onrender.com"; // βάλε το URL που θες
 
-setInterval(async () => {
-  try {
-    const response = await axios.get(url);
-    console.log("Pinged:", response.status);
-  } catch (error) {
-    console.error("Ping failed:", error.message);
-  }
-}, 5 * 60 * 1000); // κάθε 5 λεπτά
-/////////////////////////////////////////////////////////////
-
 app.get("/roblox/customersPlaces", (req, res) => {
   res.json([{114248049331485: "Available" }]);
 });
